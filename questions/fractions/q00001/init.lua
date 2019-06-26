f0cb0ee1498e70669a5cdbde24629783ff8f0a6b
bozidar <bozidar@microsoft.com>
2019-06-26 06:58:09 +0000

    Added new question

---
+++ questions/fractions/q00001/init.lua
@@ -0,0 +1,35 @@
+--[[ Use e.g. this to choose colours: https://www.w3schools.com/colors/colors_picker.asp ]]--
+
+pink = {["border"] = "1px solid",
+        ["width"] = "30px",
+        ["height"] = "30px",
+        ["text-align"] = "center",
+        ["background-color"] = "#ff99cc"};
+
+green = {["border"] = "1px solid",
+        ["width"] = "30px",
+        ["height"] = "30px",
+        ["text-align"] = "center",
+        ["background-color"] = "#66ff66"};
+
+orange = {["border"] = "1px solid",
+        ["width"] = "30px",
+        ["height"] = "30px",
+        ["text-align"] = "center",
+        ["background-color"] = "#ff9900"};
+
+blue = {["border"] = "1px solid",
+        ["width"] = "30px",
+        ["height"] = "30px",
+        ["text-align"] = "center",
+        ["background-color"] = "#00ccff"};
+
+white = {["border"] = "1px solid",
+        ["width"] = "30px",
+        ["height"] = "30px",
+        ["text-align"] = "center",
+        ["background-color"] = "#ffffff"};
+
+
+--[[ Table border ]]--
+border = {["border-collapse"] = "collapse"};
---
+++ questions/fractions/q00001/text.rs
@@ -0,0 +1,38 @@
+@h1@ 1.a Dopuni recenice @/h1@
+
+
+@lib.start_table(border)@
+  @lib.start_row()@
+    @lib.add_cell("\(\frac{1}{4}\)", pink)@
+    @lib.add_cell("", white)@
+  @lib.end_row()@
+  @lib.start_row()@
+    @lib.add_cell("", white)@
+    @lib.add_cell("", white)@
+  @lib.end_row()@
+@lib.end_table()@
+
+
+Obojena je @lib.check_value(1)@ cetvrtina.
+
+Zapisujemo razlomkom @lib.check_fraction(1, 4)@
+
+
+
+@lib.start_table(border)@
+  @lib.start_row()@
+    @lib.add_cell("\(\frac{1}{4}\)", green)@
+    @lib.add_cell("\(\frac{1}{4}\)", green)@
+  @lib.end_row()@
+  @lib.start_row()@
+    @lib.add_cell("", white)@
+    @lib.add_cell("", white)@
+  @lib.end_row()@
+@lib.end_table()@
+
+
+Obojena je @lib.check_value(2)@ cetvrtina.
+
+Zapisujemo razlomkom @lib.check_fraction(2, 4)@
+
+
