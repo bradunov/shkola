47a48ce98cc5f554d2897d08116658f51add6668
bozidar <bozidar@microsoft.com>
2019-07-06 06:20:03 +0000

    Refactored fractions (small fix)

--- questions/fractions/q00003/iter.lua
+++ questions/fractions/q00003/iter.lua
@@ -5,8 +5,8 @@ brojilac = math.random(imenilac)
 veliki_brojilac = ceo * imenilac + brojilac
 vrednost = ceo + brojilac / imenilac
 
-condition = "is_ok = (whole + numerator/denominator == "..tostring(vrednost)..");"
-condition = "is_ok = (whole * denominator + numerator == "..tostring(veliki_brojilac)..");"
+--[[ We use OR here as the frist one works well for whole numbers (e.g. 16/8) and the second one for infinite digit numbers (e.g. 10/3) ]]--
+condition = "is_ok = (whole + numerator/denominator == "..tostring(vrednost).." || whole * denominator + numerator == "..tostring(veliki_brojilac)..");"
 
 
 
