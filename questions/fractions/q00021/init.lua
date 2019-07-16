7a71d3eaa38a49c0cdb676d9d809c69034387bc9
bozidar <bozidar@microsoft.com>
2019-07-16 21:45:24 +0000

    Fixed q21

--- questions/fractions/q00021/init.lua
+++ questions/fractions/q00021/init.lua
@@ -9,4 +9,4 @@ provera =  ostalo/strane
 
 index_ime = math.random(#musko_ime_nom);    
 ime = musko_ime_nom[index_ime];                   
-padez = niz_padez[index_ime];                      
+padez = musko_ime_dativ[index_ime];                      
--- questions/fractions/q00021/text.rs
+++ questions/fractions/q00021/text.rs
@@ -4,6 +4,6 @@
 Prvog dana procitao je  \(\frac{1}{@imenilac1@}\) knjige, a 
 
 drugog dana \(\frac{1}{@imenilac2@}\) knjige. 
-Koliko je @ime@ jos ostalo da procita?
+Koliko je @padez@ jos ostalo da procita?
   
 @center@ @lib.check_fraction_condition("is_ok = math.eq(numerator/denominator, "..tostring(provera)..");")@ knjige, tj.@lib.check_number(ostalo)@  strana.
