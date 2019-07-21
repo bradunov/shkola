7ebb2912f2d89f24c98e8b9b21e96c70854ee237
bozidar <bozidar@microsoft.com>
2019-07-21 09:37:44 +0000

    Improved question 25

--- questions/fractions/q00025/iter.lua
+++ questions/fractions/q00025/iter.lua
@@ -1,4 +1,17 @@
-
 style_table_current = style_table
 style_table_current["color"] = array_boja[ITEM]
 
+
+array_table = {};
+enum = 0;
+denom = 5+ITEM;
+for i=1,denom do
+  if math.random() > 0.5 then
+    array_table[i] = 1
+    enum = enum + 1
+  else
+    array_table[i] = 0
+  end
+end
+
+style_table_current["initial_state"] = array_table
--- questions/fractions/q00025/text.rs
+++ questions/fractions/q00025/text.rs
@@ -1,6 +1,8 @@
 Koji deo figure je obojen? Zapisi razlomkom.
 
 @repeat(4)@
-@center@ @lib.select_objects(5+ITEM, 1, "table", "", style_table_current)@ \
-== @lib.check_fraction_condition("is_ok = (numerator/denominator == 1/6)")@
+
+@center@ @lib.select_objects(denom, 1, "table", "", style_table_current)@ \
+== @lib.check_fraction_condition(\
+    "is_ok = (numerator/denominator == "..tostring(enum).."/"..tostring(denom)..")")@
 @/repeat@
