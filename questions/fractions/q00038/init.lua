de1918389aaec266201ee7e63ebef6af92041beb
bozidar <bozidar@microsoft.com>
2019-08-25 13:01:27 +0000

    Fixed a few bugs in questions

--- questions/fractions/q00038/init.lua
+++ questions/fractions/q00038/init.lua
@@ -2,7 +2,7 @@ include("imena")
 
 index_ime = math.random(#musko_ime_nom);    
 ime = musko_ime_nom[index_ime];                   
-padez = niz_padez[index_ime]; 
+padez = musko_ime_gen[index_ime]; 
 
 deo = math.random(5) + 1;
 put1 = math.random(deo - 1);
