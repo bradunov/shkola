7cefa61692db899bd42d971397911f383ff9ff70
bozidar <bozidar@microsoft.com>
2019-08-25 13:03:24 +0000

    Fixed a few bugs in questions

--- questions/fractions/q00013a/iter.lua
+++
@@ -1,5 +0,0 @@
-x = arrayX[ITEM]
-y = arrayY[ITEM]
-type = arrayType[ITEM]
-denom = arrayDenom[ITEM]
-res = x*y / denom 
--- questions/fractions/q00013a/text.rs
+++
@@ -1,6 +0,0 @@
-@h1@Oboj deo figure zapisan razlomkom @/h1@
-
-@repeat(3)@
-@center@ \(\frac{1}{@denom@}\) = \
-@center@ @lib.select_objects(x, y, type, "sum(result) == "..tostring(res), style)@
-@/repeat@
