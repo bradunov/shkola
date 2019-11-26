f277255a42c94550decb4799fc1098b25c3e5b02
bozidar <bozidar@microsoft.com>
2019-11-26 07:25:34 +0000

    More questions

--- questions/fractions/q00052/text.rs
+++ questions/fractions/q00052/text.rs
@@ -1,8 +1,8 @@
-@ime@ je na pijac donela  @ukupno@ kg jabuka. Pre podne je prodala  \(\frac{1}{@imenilac1@}\) jabuka , a posle podne \(\frac{1}{@imenilac2@}\) od preostalih jabuka. 
+@ime@ je na pijac doneo  @ukupno@ kg jabuka. Pre podne je prodao  \(\frac{1}{@imenilac1@}\) jabuka , a posle podne \(\frac{1}{@imenilac2@}\) od preostalih jabuka. 
 
 @vspace@
 
-Koliko kilograma jabuka @ime@ nije prodala? @lib.check_number(ostalo)@   
+Koliko kilograma jabuka @ime@ nije prodao? @lib.check_number(ostalo)@   
                  
 
 
---
+++ questions/number/q00007/text.rs
@@ -0,0 +1,12 @@
+Uporedi broj crvenih i plavih kvadratića.
+Upiši znak @hspacept(8)@<@hspacept(4)@, @hspacept(8)@> @hspacept(8)@ili @hspacept(8)@ =@hspacept(4)@.
+
+@center@ @mycanvas(no)@
+
+@center@@lib.check_string(sign)@
+                  
+                  
+                            
+                  
+
+
