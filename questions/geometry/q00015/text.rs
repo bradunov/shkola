3f6411bcc39402b34c51b4647fde27ec8f4d932b
Bozidar Radunovic <bozidar.radunovic@gmail.com>
2019-12-21 14:34:45 +0100

    Added new question

---
+++ questions/geometry/q00015/text.rs
@@ -0,0 +1,5 @@
+Polazeći od strelice na prvoj raskrsnici idi @position[1]@, na sledećoj raskrsnici
+idi @position[2]@, i zatim  @position[3]@.
+Nastavi da bojiš putanju koju prolaziš.
+
+@center@ @mycanvas(results)@
