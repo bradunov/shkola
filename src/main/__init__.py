ef28abbedbdef0d7de16dd0b0079ecfff16b0765
Bozidar Radunovic <bozidar.radunovic@gmail.com>
2019-12-27 15:02:54 +0100

    Fixed import path in Azure function environment

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -1,7 +1,14 @@
 import logging
 
+import os
 import sys
-sys.path.append(".")
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
+
+logging.warning("System path: " + str(sys.path))
+
+
+
 from server import page
 from server import question
 
