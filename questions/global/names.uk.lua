cf0c5ebf2bad77546a336d261c4337f4bf860ec4
Bozidar Radunovic <bozidar.radunovic@gmail.com>
2020-03-29 19:11:05 +0100

    Updated global english names

--- questions/global/names.uk.lua
+++ questions/global/names.uk.lua
@@ -1,2 +1,20 @@
 names = {"Peter", "Thomas", "Ben", "Oliver", "Charlie", "Darragh", "Oren", "Leo", 
          "Anna", "Clara", "Hannah", "Arabella", "Lois", "Abigale", "Phoebe"}
+
+musko_ime_nom = {"Peter", "Thomas", "Ben", "Oliver", "Charlie", "Darragh", "Oren", "Leo", "Luke", "Aran", "Felix"};
+musko_ime_gen = musko_ime_nom;
+musko_ime_dativ = musko_ime_nom;
+musko_ime_akuzativ = musko_ime_nom;
+musko_ime_instrum = musko_ime_nom;
+musko_ime_lokativ = musko_ime_nom;
+
+musko_pridev = musko_ime_nom;
+
+zensko_ime_nom = {"Anna", "Clara", "Hannah", "Arabella", "Lois", "Abigale", "Phoebe"}; 
+zensko_ime_gen = zensko_ime_nom;
+zensko_ime_dativ = zensko_ime_nom;
+zensko_ime_akuzativ = zensko_ime_nom;
+zensko_ime_instrum = zensko_ime_nom;
+zensko_ime_lokativ = zensko_ime_nom;
+
+zensko_pridev = zensko_ime_nom;
