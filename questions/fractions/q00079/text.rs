a176b520b7010fbc6d526f5c7f3cf325065eb4f8
Bozidar Radunovic <bozidar.radunovic@gmail.com>
2022-09-02 13:51:56 +0200

    Update of questions and lists

--- questions/fractions/q00079/text.rs
+++ questions/fractions/q00079/text.rs
@@ -2,8 +2,10 @@
 @ime@ ima @number@ @pad@. Svakog dana pojede do \(\frac{@1@}{@fact@}\) kajsija koje su preostale, jer pojede samo ceo deo od izračunatog broja kajsija. Preostale kajsije ostavlja za sutradan. Poslednjeg dana pojede sve preostale kajsije.
 
  @vspace@                
-(Primer: Ana   pojede 1/3 od 7 kajsija tako što  pojede 2, a za sutradan joj ostanu 3. Drugi dan pojede 1/3 od preostale tri kajsije (jednu) i ostanu joj 2. Treći dan pojede preostale dve kajsije, jer je 1/3 od dve kajsije manje od cele kajsije.)
- @vspace@       
+(Primer: Ana   pojede do 1/3 od 5 kajsija tako što  pojede 1, a za sutradan joj ostanu 4 kajsije. Drugi dan pojede do 1/3 od preostale četiri kajsije (jednu) i ostanu joj 3. Treći dan pojede do 1/3 od tri kajsije (jednu), a za sutradan joj ostanu 2 kajsije. Četvrti dan pojede preostale dve kajsije, jer je 1/3 od dve kajsije manje od cele kajsije.)
+@vspace@       
 Za koliko  dana će @ime@ pojesti sve kajsije? 
 
 @center@ @ime@ će pojesti sve kajsije za @lib.check_number(nr)@ dana.
+
+       
